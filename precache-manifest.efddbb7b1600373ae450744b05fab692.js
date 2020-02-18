eae566a207cc564198b0ff65a6153b9f308c29f5
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-02-18 14:07:32 +0000

    Updates

---
+++ precache-manifest.efddbb7b1600373ae450744b05fab692.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804730dd824dbfe8ec5ac044cd01431d",
+    "url": "/test-react-site/index.html"
+  },
+  {
+    "revision": "637a3a4626a41f2cf0c0",
+    "url": "/test-react-site/static/css/2.67d9df46.chunk.css"
+  },
+  {
+    "revision": "11c2fbd90c7b64905d23",
+    "url": "/test-react-site/static/css/main.79692053.chunk.css"
+  },
+  {
+    "revision": "637a3a4626a41f2cf0c0",
+    "url": "/test-react-site/static/js/2.77eb4b32.chunk.js"
+  },
+  {
+    "revision": "11c2fbd90c7b64905d23",
+    "url": "/test-react-site/static/js/main.b28c3a3f.chunk.js"
+  },
+  {
+    "revision": "ea244e4c0ba74950d68f",
+    "url": "/test-react-site/static/js/runtime-main.ac5f4cf9.js"
+  },
+  {
+    "revision": "980d978973ac9270bd048c3d6e006a76",
+    "url": "/test-react-site/static/media/api.980d9789.yaml"
+  },
+  {
+    "revision": "ecc65d60f605c12d3fca79c517dc4195",
+    "url": "/test-react-site/static/media/download-icon-white.ecc65d60.svg"
+  },
+  {
+    "revision": "f5e25df7970ccd13967cf3ba2e766855",
+    "url": "/test-react-site/static/media/drop-grey.f5e25df7.svg"
+  },
+  {
+    "revision": "78cd02a100de1ab70ac31455baa11fa2",
+    "url": "/test-react-site/static/media/drop-white.78cd02a1.svg"
+  },
+  {
+    "revision": "34536746f7b8c5b46d8675f1941e1dbc",
+    "url": "/test-react-site/static/media/info-icon.34536746.svg"
+  },
+  {
+    "revision": "f2df34b5849355fed739e6a90915203a",
+    "url": "/test-react-site/static/media/logo.f2df34b5.svg"
+  },
+  {
+    "revision": "d41c631875217e983fe2051e2c62932e",
+    "url": "/test-react-site/static/media/modal-close.d41c6318.svg"
+  },
+  {
+    "revision": "b01fa7f07a4c2e355eff2be5069f4c93",
+    "url": "/test-react-site/static/media/montserrat-regular-webfont.b01fa7f0.woff2"
+  },
+  {
+    "revision": "e8b7d8acf640a69f838ca38c063e33ff",
+    "url": "/test-react-site/static/media/montserrat-regular-webfont.e8b7d8ac.woff"
+  },
+  {
+    "revision": "9d9e09d7ae09f64a8a365e669105bedd",
+    "url": "/test-react-site/static/media/roboto-regular-webfont.9d9e09d7.woff"
+  },
+  {
+    "revision": "d96d07c1e0f7cdf7a1988dcb0bf6e0e4",
+    "url": "/test-react-site/static/media/roboto-regular-webfont.d96d07c1.woff2"
+  }
+]);
