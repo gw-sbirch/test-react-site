efa4e6d651c18c2700c3ece4290ca0bc53329f71
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-02-18 08:51:14 +0000

    mirror real repo

--- src/components/fileTypeDetectionSwag.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SwaggerUI from "swagger-ui-react"
-import yam from "../api.yaml"; 
-import "../Swagger.css";
-
-var FileTypeDetectionSwag = () => <SwaggerUI url={window.location.origin + yam} />
-
-export default FileTypeDetectionSwag;
